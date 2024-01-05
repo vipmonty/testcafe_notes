db5aff2db02ea6c177b8bb6da12c2d3a972fade9
Viphakone Monty <10835390@my.uvu.edu>
2024-01-04 20:13:54 -0700

    functioning firstTest.js updates

--- fixtures/firstTest.js
+++ fixtures/firstTest.js
@@ -1,12 +1,52 @@
-fixture("Vips first fixture")
+import { Selector, t } from "testcafe";
+
+fixture("Vips first fixture").skipJsErrors();
 
 test
+    .skip
     .page("youtube.com")
     ("vip's first test", async t => {
         console.log("Pass!!!!")
     });
+
 test
-    .page('google.com')
-    ('googot', async => {
-        console.log("googot passed too")
+    .skip
+    .page('http://demoqa.com/text-box')
+    ("demoaqButtonTest", async t => {
+        const button = Selector('#item-3');
+        await t.click(button).wait(5000)
+
+        const headerSelector = Selector(".main-header");
+        const headerString = await headerSelector.innerText;
+
+        console.log('Header String: ' + headerString);
+    });
+
+test
+    .page("youtube.com")
+    ("youtube_test", async t => {
+        const name_test = Selector('.ytd-searchbox');
+        await t
+            .click(name_test).wait(5000)
+
     })
+
+// test
+
+//     .page('google.com')
+//     ('google search button test', async t => {
+//         console.log("googot passed too")
+//         const search_button = Selector("input.gNO89b")
+//         await t.click(search_button)
+
+//     })
+// test
+//     .page('https://vipmonty.github.io/portfolio_project.github.io/')
+//     ('Testing my git.io page', async t => {
+//         console.log("Testing if my vip section button works")
+//         const vip_button = Selector('#navPanelToggle')
+//         await t
+//             .click(vip_button)
+//             .wait(100000)
+
+// })
