98a0e0d13276ffe9305a3b4ce3fa11e4ce48c139
Viphakone Monty <10835390@my.uvu.edu>
2024-01-06 21:47:11 -0700

    1/6/24

--- fixtures/firstTest.js
+++ fixtures/firstTest.js
@@ -12,17 +12,18 @@ test
 test
     .skip
     .page('http://demoqa.com/text-box')
-    ("demoaqButtonTest", async t => {
-        const button = Selector('#item-3');
-        await t.click(button).wait(5000)
+    ("demoaqelementTest", async t => {
+        const element = Selector('#item-3');
+        await t.click(element).wait(5000)
 
-        const headerSelector = Selector(".main-header");
+        const headerSelector = Selector(".rt-resizable-header-content");
         const headerString = await headerSelector.innerText;
 
-        console.log('Header String: ' + headerString);
+        console.log('inner Text is: ' + headerString);
     });
 
 test
+    .skip
     .page("youtube.com")
     ("youtube_test", async t => {
         const name_test = Selector('.ytd-searchbox');
@@ -31,22 +32,32 @@ test
 
     })
 
+test
+    .page('youtube.com')
+    ('searchBarTest', async t => {
+        const element = Selector('.ytd-feed-filter-chip-bar-renderer')
+        await t.click(element).wait(5000)
+        const innerText = await element.classNames
+        console.log('here are the attributes ' + innerText)
+    })
+
+
 // test
 
 //     .page('google.com')
-//     ('google search button test', async t => {
+//     ('google search element test', async t => {
 //         console.log("googot passed too")
-//         const search_button = Selector("input.gNO89b")
-//         await t.click(search_button)
+//         const search_element = Selector("input.gNO89b")
+//         await t.click(search_element)
 
 //     })
 // test
 //     .page('https://vipmonty.github.io/portfolio_project.github.io/')
 //     ('Testing my git.io page', async t => {
-//         console.log("Testing if my vip section button works")
-//         const vip_button = Selector('#navPanelToggle')
+//         console.log("Testing if my vip section element works")
+//         const vip_element = Selector('#navPanelToggle')
 //         await t
-//             .click(vip_button)
+//             .click(vip_element)
 //             .wait(100000)
 
 // })
